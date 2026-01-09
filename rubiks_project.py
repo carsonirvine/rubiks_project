f85b9c6077c8e9929027fd1dcc6a92539e3800ff
carsonirvine <carsonspencerirvine@gmail.com>
2026-01-08 20:50:55 -0800

    Split information from main file to txt file

--- rubiks_project.py
+++ rubiks_project.py
@@ -1,13 +1,49 @@
-import sys
-#sys.path.append("C:/Users/Carson/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0/LocalCache/local-packages/Python313/site-packages/")
-
 import magiccube
 from magiccube import BasicSolver
 import random
 from scramble import Scramble
 
 
-piece_locations = [[[[0 for x in range(6)] for i in range(6)] for y in range(6)] for k in range(24)] # creates the 4d array storing each piece location based on its three face colours
+corner_piece_to_letter = {
+    "OYB": "H",
+    "YBO": "X",
+    "BOY": "S",
+
+    "OYG": "G",
+    "YGO": "U",
+    "GOY": "L",
+
+    "OWB": "E",
+    "WBO": "A",
+    "BOW": "R",
+
+    "OWG": "F",
+    "WGO": "D",
+    "GOW": "I",
+
+    "RYB": "O",
+    "YBR": "W",
+    "BRY": "T",
+
+    "RYG": "P",
+    "YGR": "V",
+    "GRY": "K",
+
+    "RWB": "N",
+    "WBR": "B",
+    "BRW": "Q",
+
+    "RWG": "M",
+    "WGR": "C",
+    "GRW": "J"
+}
+
+corner_letter_to_piece = {v: k for k, v in corner_piece_to_letter.items()}
+
+edge_piece_locations = {
+    "OY": "G",
+    "YO": "X"
+}
 
 
 moves = ["R","R'","R2","L","L'","L2","U","U'","U2","D","D'","D2","F","F'","F2","B","B'","B2"]
@@ -19,27 +55,12 @@ def print_all_pieces():
                 print(i,",",j,",",k,": ", cube.get_piece((i,j,k)), "\n")
 
 '''
-For piece coordinates, starting with green front and white top (x,y,z)
-first digit X reperesents column from front left to right
-second digit z represents row from front bottom to top          
-third digit y represents layer from back to front
 
-back bottom left is 0,0,0
-core is 1,1,1
-top front right is 2,2,2
-
-get_piece returns the piece as a string from position x,y,z.
-If x is 0 or 2 the first letter will be what is on either the left or right faces
-If x is 1 then there will only be maximum two letters because in the core column
-
-If numbers contains only 0s or 2s its a corner.
-If contains one 1 its a edge
-If it has multiple 1s its a center piece
 
 '''
 
 
-cube = magiccube.Cube(3,"WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRBRRRBBBRBBBBBYYYYYYYYY")
+cube = magiccube.Cube(3,"WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY")
 
 solver = BasicSolver(cube)
 
@@ -49,35 +70,17 @@ print(scramble.scramble)
 
 
 
-
 print(cube)
-print("Finding RB edge: ", cube.find_piece("RB"))
-print("Finding BR edge: ", cube.find_piece("BR"))
-print("piece 2,1,0: ", cube.get_piece((2,1,0)))
+for n in range(3):
+    for i in range(3):
+        for k in range(3):
+            print(f"piece {n},{i},{k}: ", cube.get_piece((n,i,k)))
+
+
+cube.rotate(scramble.scramble)
+print("\nSCRAMBLING\n")
 print(cube)
-#print(scramble)
-#cube.rotate(scramble)
-#print(cube)
-'''
-print("-1,-1,-1",cube.get_piece((-1,-1,-1)),"\n")
-print("-1,-1, 0",cube.get_piece((-1,-1,0)),"\n")
-print("-1,-1, 1",cube.get_piece((-1,-1,1)),"\n")
-print("-1,-1,-1",cube.get_piece((-1,-1,-1)),"\n")
-print("-1, 0,-1",cube.get_piece((-1,0,-1)),"\n")
-print("-1, 1,-1",cube.get_piece((-1,1,-1)),"\n")
-print("0,0,0",cube.get_piece((0,0,0)),"\n")
-print("1,1,1",cube.get_piece((1,1,1)),"\n")
-'''
-
-#print_all_pieces()
-
-'''
-print("ALL PIECES: ", cube.get_all_pieces())
-cube.rotate(scramble)
-print("ALL PIECES: ", cube.get_all_pieces())
-'''
-
-print("Finding RB edge: ", cube.find_piece("RB"))
-print("Finding BR edge: ", cube.find_piece("BR"))
-print("piece 2,1,0: ", cube.get_piece((1,0,0)))
-
+for n in range(3):
+    for i in range(3):
+        for k in range(3):
+            print(f"piece {n},{i},{k}: ", cube.get_piece((n,i,k)))
